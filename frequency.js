a3bb7f2f73d54e637001c0076687460cea69177e
BThomas <bran_thom@yahoo.com>
2025-01-15 12:35:08 -0600

    max char

--- frequency.js
+++ frequency.js
@@ -1,21 +1,24 @@
-function maxChar(str){
-let hashtable = {}
+function maxChar(str) {
+    let hashtable = {};
+    let max = 0;
+    let maxChar = "";
 
-for (let char in str){
-    if (!hashtable[str[char]]){
-        hashtable[str[char]] = 1
+    for (let char of str) {
+        if (hashtable[char]) {
+            hashtable[char] += 1;
+        } else {
+            hashtable[char] = 1;
+        }
     }
-    else{
-        hashtable[str[char]] += 1
+
+    for (let letter in hashtable) {
+        if (hashtable[letter] > max) {
+            max = hashtable[letter];
+            maxChar = letter;
+        }
     }
+    return maxChar;
 }
-
-for (let letter in hashtable){
-    if (hashtable[letter]> 1)
-        return letter
-}
-
-}
-console.log(maxChar("appple"))
-console.log(maxChar(" appple 12111115"))
-console.log(maxChar("abccccccccd"))
+console.log(maxChar("appple"));
+console.log(maxChar(" appple 12111115"));
+console.log(maxChar("abccccccccd"));
