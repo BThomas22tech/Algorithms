3bf21cc1057a2045bb658e59bf81b2af19f66760
BThomas <bran_thom@yahoo.com>
2025-01-15 10:35:54 -0600

    reverse number

--- reversedString.js
+++ reversedString.js
@@ -1,9 +1,43 @@
+// function reverse(str){
+//     let reversed = ""
+//     for ( let char of str){ 
+//         reversed = char + reversed
+//     }
+// return reversed
+// }
+
+// console.log(reverse("abc"))
+
+
+// Builtin reverse ##############
+// function reverse(str){
+//     const strToArray = str.split("")
+
+//     strToArray.reverse()
+//     return strToArray.join('')
+
+// }
+
+// console.log(reverse('CodingMoney'))
+
+// chain reverse################
+
 function reverse(str){
-    let reversed = ""
-    for ( let char of str){ 
-        reversed = char + reversed
-    }
-return reversed
+    return str.split('').reverse().join('')
+}
+// console.log(reverse('CodingMoney'))
+
+
+
+// ######### given an integer, return integer that is the reverse ordering of numbers
+
+function reverseInt(n){
+    let newNumb = ""
+    n = n.toString()
+for (let number in n){
+    newNumb = n[number] + newNumb
+}
+return parseInt(newNumb) * Math.sign(n)
 }
 
-console.log(reverse("abc"))
+console.log(reverseInt(-51))
