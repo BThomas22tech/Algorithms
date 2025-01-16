f5edd5142ea167eea32edc2c10a3b8501fbabbb0
BThomas <bran_thom@yahoo.com>
2025-01-16 14:32:49 -0600

    step shift

---
+++ stepshift.js
@@ -0,0 +1,13 @@
+function steps(n){
+    for (let row = 1; row<=n;row++){
+        let line = ""
+        for (let col = 1; col<=n;col++){
+            if (col<=row){
+                line +='#'
+            }
+        }
+        console.log(line)
+    }
+}
+
+steps(3)
